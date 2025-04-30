6e630a4f8668c440acf66fca12813dec90950988
juninishihara <juni.nishihara@gmail.com>
2025-04-30 15:34:35 +0900

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,23 +1,14 @@
-// coi-serviceworker.js
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
-
 self.addEventListener('fetch', event => {
   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-
   event.respondWith(
     fetch(event.request)
       .then(res => {
         const newHeaders = new Headers(res.headers);
         newHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
         newHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
-
-        return new Response(res.body, {
-          status: res.status,
-          statusText: res.statusText,
-          headers: newHeaders
-        });
+        return new Response(res.body, { status: res.status, headers: newHeaders });
       })
-      .catch(e => console.error(e))
   );
 });
